9ac320d9640d6580201417a80257a4a0908048d4
A9856 <adilkhanofficial74@gmail.com>
2025-07-12 17:55:56 +0530

    package.json App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import ManageEmployees from './ManageEmployees';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/S-Soluution/'>
     <Navbar/>      
       <Routes>
         <Route path="/" element={<Home/>} />
